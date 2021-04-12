09beda20bf2768e73ac915f9477ccc123aeb09a4
Geoff Rich <geoff.rich@alaskaair.com>
2021-04-11 21:45:54 -0700

    Remove Step

--- src/Step.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  export let source;
-  export let destination;
-  export let disk;
-</script>
-
-<li>Move disk {disk} from {source} to {destination}</li>
--- src/Tower.svelte
+++ src/Tower.svelte
@@ -1,16 +1,11 @@
 <script>
-  export let source;
-  export let intermediate;
-  export let destination;
-  export let disk;
-
-  import Step from './Step.svelte';
+	export let disk, source, intermediate, destination;
 </script>
 
 {#if disk === 1}
-<Step {disk} {source} {destination} />
+<li>Move disk {disk} from {source} to {destination}</li>
 {:else}
 <svelte:self disk={disk - 1} source={source} intermediate={destination} destination={intermediate} />
-<Step {disk} {source} {destination} />
+<li>Move disk {disk} from {source} to {destination}</li>
 <svelte:self disk={disk - 1} source={intermediate} intermediate={source} destination={destination} />
 {/if}
